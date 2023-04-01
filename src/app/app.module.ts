c8858d839ae6ba9a737b6571d51317d09a33719e
gilvan alves <gilvan.office@gmail.com>
2023-04-01 11:59:52 -0300

    Navbar adcionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replice.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent
   ],
   imports: [
 
